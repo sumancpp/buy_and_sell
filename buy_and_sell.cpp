5421fa5394ec2b879d0e5be9775c150d04072d72
Suman Maity <someone.cpp@proton.me>
2025-03-19 23:18:33 +0530

    buy_and_sell.cpp

--- buy_and_sell.cpp
+++ buy_and_sell.cpp
@@ -1,15 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int price[]={7,1,2,6,4,5};
-    int size=6;
-    int best_buy=price[0];
-    int max_profit=0;
-    for(int i=0;i<size;i++){
-        if(price[i]>best_buy){
-         max_profit=max(max_profit,price[i]-best_buy);
+int main () {
+    int price[] = {7, 1, 2, 6, 4, 5};
+    int size = 6;
+    int best_buy = price[0];
+    int max_profit = 0;
+    for(int i = 0; i < size; i++){
+        if(price[i] > best_buy){
+         max_profit = max(max_profit, price[i] - best_buy);
         }
-      best_buy=min(best_buy,price[i]);
+      best_buy = min(best_buy, price[i]);
     }
      cout<<"The best buy is "<<max_profit;
     return 0;
